a10e37fadecf84440f33d25b9a9e163b07fb0c70
LeoSanGo <souoleo@gmail.com>
2021-03-14 15:59:51 -0300

    Adicionado formato .njk e criado data.js

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "GykTLqODQuU",
+        title: "Formulario animado com JS puro e CSS Animation | Mayk Brito",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o CSS grid na pratica | Mayk Brito",
+        duration: "36 min",
+        price: "Free"
+    },
+    {
+        id: "rAzHvYnQ8DY",
+        title: "High order Functions | Mayk Brito",
+        duration: "54 min",
+        price: "Free"
+    },
+    {
+        id: "ghTrp1x_1As",
+        title: "FoO que é API? RESR e RESTful | Mayk Brito",
+        duration: "33 min",
+        price: "Free"
+    },
+    {
+        id: "GSqR2i-Pq6o",
+        title: "Desvendando a variavel THIS no Javascript | Mayk Brito",
+        duration: "48 min",
+        price: "Free"
+    },
+    {
+        id: "2alg7MQ6_sI",
+        title: "Formulario animado com JS puro e CSS Animation | Mayk Brito",
+        duration: "33 min",
+        price: "Free"
+    }
+]
