78ef073a51ec7cea106c1ef5bea35de5fdbce822
LeoSanGo <souoleo@gmail.com>
2021-03-14 15:00:27 -0300

    Adicionado o Nunjucks, index, portifolio e rotas

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
 
+server.use(express.static('public'))
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express: server
+
+})
+
 server.get("/", function(req, res){
-    return res.send("Hi- Nodemon")
+    return res.render("index")
+})
+
+server.get("/portifolio", function(req, res){
+    return res.render("portifolio")
 })
 
 server.listen(5000, function() {
